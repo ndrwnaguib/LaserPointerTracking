e976cb80aae99f42eca6a6507262fb6a83de9d3a
Andrew Nagyeb <andrewnajeeb@fci.helwan.edu.eg>
2017-07-04 20:21:39 +0200

    First Commit

---
+++ track_laser.py
@@ -0,0 +1,36 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+
+while(1):
+
+    # Take each frame
+    _, frame = cap.read()
+
+    # Convert BGR to HSV
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    # define range of blue color in HSV
+    lower_red = np.array([170, 50, 50])
+    upper_red = np.array([180, 255, 255])
+
+    # Threshold the HSV image to get only blue colors
+    mask = cv2.inRange(hsv, lower_red, upper_red)
+
+    # Bitwise-AND mask and original image
+    res = cv2.bitwise_and(frame,frame, mask= mask)
+    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+                            cv2.CHAIN_APPROX_SIMPLE)[-2]
+    c = max(cnts, key=cv2.contourArea)
+    ((x, y), radius) = cv2.minEnclosingCircle(c)
+    M = cv2.moments(c)
+    center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+    cv2.circle(frame, center, 30, (0, 0, 0), 2)
+
+    cv2.imshow('frame',frame)
+    k = cv2.waitKey(5) & 0xFF
+    if k == 100:
+        break
+
+cv2.destroyAllWindows()
